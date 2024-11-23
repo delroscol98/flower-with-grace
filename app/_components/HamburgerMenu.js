619635a057a7df5d055fcb05556a41a04908ab3e
delroscol98 <collindelrosario98@gmail.com>
2024-11-23 14:06:59 +1100

    animating hamburger menu

--- app/_components/HamburgerMenu.js
+++ app/_components/HamburgerMenu.js
@@ -1,5 +1,27 @@
+"use client";
+
+import { useRef, useState } from "react";
+
 function HamburgerMenu() {
-  return <div className="hamburgerMenu mr-3"></div>;
+  const [isChecked, setIsChecked] = useState(false);
+
+  const toggleHamburgerMenu = () => {
+    setIsChecked((isChecked) => !isChecked);
+  };
+
+  return (
+    <article
+      className={`${
+        isChecked ? "open" : null
+      } flex items-center px-1 py-2 h-6 mr-3 hover:cursor-pointer`}
+      onClick={toggleHamburgerMenu}
+    >
+      <input type="checkbox" id="hamburgerMenu__checkbox" hidden />
+      <label htmlFor="hamburgerMenu__checkbox">
+        <span onClick={toggleHamburgerMenu} className="hamburgerMenu"></span>
+      </label>
+    </article>
+  );
 }
 
 export default HamburgerMenu;
