9569a633de128744bdb02ea4d7005b878ec79579
delroscol98 <collindelrosario98@gmail.com>
2025-01-02 21:29:08 +1100

    navigation more semantic

--- app/_components/NavMenu.js
+++ app/_components/NavMenu.js
@@ -10,35 +10,37 @@ function NavMenu() {
     <section
       className={`top-0 bg-neutral-beige w-full h-full text-center z-10 grid items-center justify-center fixed`}
     >
-      <ul
-        className={`${rufina.className} flex flex-col gap-5 text-heading-md text-accent-dark-brown`}
-      >
-        <li>
-          <Link href="/" onClick={toggleNavMenu}>
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link href="/about" onClick={toggleNavMenu}>
-            About Us
-          </Link>
-        </li>
-        <li>
-          <Link href="/gallery" onClick={toggleNavMenu}>
-            Gallery
-          </Link>
-        </li>
-        <li>
-          <Link href="/weddings" onClick={toggleNavMenu}>
-            Weddings
-          </Link>
-        </li>
-        <li>
-          <Link href="/contact" onClick={toggleNavMenu}>
-            Contact Us
-          </Link>
-        </li>
-      </ul>
+      <nav>
+        <ul
+          className={`${rufina.className} flex flex-col gap-5 text-heading-md text-accent-dark-brown`}
+        >
+          <li>
+            <Link href="/" onClick={toggleNavMenu}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link href="/about" onClick={toggleNavMenu}>
+              About Us
+            </Link>
+          </li>
+          <li>
+            <Link href="/gallery" onClick={toggleNavMenu}>
+              Gallery
+            </Link>
+          </li>
+          <li>
+            <Link href="/weddings" onClick={toggleNavMenu}>
+              Weddings
+            </Link>
+          </li>
+          <li>
+            <Link href="/contact" onClick={toggleNavMenu}>
+              Contact Us
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </section>
   );
 }
