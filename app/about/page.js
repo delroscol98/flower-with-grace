e5e13c16f67ccc282a6348336e1d3ad14cd168d9
delroscol98 <collindelrosario98@gmail.com>
2025-01-23 13:28:50 +1100

    Photos added to the gallery page

--- app/about/page.js
+++ app/about/page.js
@@ -7,127 +7,124 @@ import whoAreWe1 from "@/public/who-are-we-1.jpeg";
 import whoAreWe2 from "@/public/who-are-we-2.jpeg";
 import RoundedText from "../_components/RoundedText";
 import CtaButton from "../_components/CtaButton";
-import Footer from "../_components/Footer";
 
 export default function AboutPage() {
   return (
-    <>
-      <section className="bg-accent-light-brown pt-6 px-4 pb-8">
-        <article className="flex flex-col gap-4 mb-14">
-          <h1
-            className={`${rufina.className} uppercase text-neutral-white text-heading-md`}
-          >
-            Hi! I&apos;m Abigail
-          </h1>
-          <h2
-            className={`${rufina.className} text-heading-sm text-neutral-white`}
-          >
-            Founder and Creative Director of Flower with Grace!
-          </h2>
-        </article>
-
-        <Image
-          className="rounded-t-full rounded-b-full h-[555px] object-cover object-left"
-          src={about1}
-          alt="Flower with Grace owner and creative director carefully arranges a hanging installation of flowers on a blank white wall behind a white table that has been decorated with candles and vases filled with flowers."
-          priority={true}
-        />
-
-        <article
-          className={`${averageSans.className} text-paragraph text-neutral-white grid gap-4 mt-10 mb-10`}
+    <section className="bg-accent-light-brown pt-6 px-4 pb-8">
+      <article className="flex flex-col gap-4 mb-14">
+        <h1
+          className={`${rufina.className} uppercase text-neutral-white text-heading-md`}
         >
-          <p>
-            I&apos;ve always loved all things flowers, design and styling,
-            whether it be curating a bouquet to brighten up my home or using my
-            creativity to style a friend&apos;s special event.
-          </p>
-          <p>
-            Since having my baby girl, I&apos;ve decided to branch out from my
-            career in healthcare to further develop my creative skills and
-            pursue my passion for floristry!
-          </p>
-          <p>
-            After working with the wonderful wedding florist & stylist, Kristela
-            Marie, I decided to take the leap and bring my dream to life through
-            the creation of Flower with Grace!
-          </p>
-        </article>
+          Hi! I&apos;m Abigail
+        </h1>
+        <h2
+          className={`${rufina.className} text-heading-sm text-neutral-white`}
+        >
+          Founder and Creative Director of Flower with Grace!
+        </h2>
+      </article>
 
+      <Image
+        className="rounded-t-full rounded-b-full h-[555px] object-cover object-left"
+        src={about1}
+        alt="Flower with Grace owner and creative director carefully arranges a hanging installation of flowers on a blank white wall behind a white table that has been decorated with candles and vases filled with flowers."
+        priority={true}
+      />
+
+      <article
+        className={`${averageSans.className} text-paragraph text-neutral-white grid gap-4 mt-10 mb-10`}
+      >
+        <p>
+          I&apos;ve always loved all things flowers, design and styling, whether
+          it be curating a bouquet to brighten up my home or using my creativity
+          to style a friend&apos;s special event.
+        </p>
+        <p>
+          Since having my baby girl, I&apos;ve decided to branch out from my
+          career in healthcare to further develop my creative skills and pursue
+          my passion for floristry!
+        </p>
+        <p>
+          After working with the wonderful wedding florist & stylist, Kristela
+          Marie, I decided to take the leap and bring my dream to life through
+          the creation of Flower with Grace!
+        </p>
+      </article>
+
+      <article className="flex flex-col gap-10">
+        <Image
+          className="rounded-t-full h-[500px] object-cover"
+          src={about2}
+          alt="Flower with grace owner stands smiling next to a large floral arangement."
+        />
+        <Image
+          className="rounded-b-full h-[500px] object-cover"
+          src={about3}
+          alt="Flower with grace owner smiles as she finishes final touches of large floral arrangment."
+        />
+      </article>
+      <section className="mb-10">
+        <h3
+          className={`${rufina.className} text-heading-md text-neutral-white mt-14`}
+        >
+          Who are we?
+        </h3>
         <article className="flex flex-col gap-10">
-          <Image
-            className="rounded-t-full h-[500px] object-cover"
-            src={about2}
-            alt="Flower with grace owner stands smiling next to a large floral arangement."
-          />
-          <Image
-            className="rounded-b-full h-[500px] object-cover"
-            src={about3}
-            alt="Flower with grace owner smiles as she finishes final touches of large floral arrangment."
-          />
-        </article>
-        <section className="mb-10">
-          <h3
-            className={`${rufina.className} text-heading-md text-neutral-white mt-14`}
-          >
-            Who are we?
-          </h3>
-          <article className="flex flex-col gap-10">
-            <article className="relative">
-              <Image
-                className="rounded-t-full w-[224px] h-[370px] object-cover object-right"
-                src={whoAreWe1}
-                alt="A bride and groom stand intimately close to each other whilst looking into the other's eyes and holding a bouquet of white flowers."
-              />
-              <RoundedText
-                fill={"#fff"}
-                xPosition={"right-[-1rem]"}
-                yPosition={"top-[2rem]"}
-              />
-            </article>
-
-            <p className={`${averageSans.className} text-neutral-white`}>
-              Based in North-West Sydney, Flower with Grace is a small yet
-              highly dedicated team focused on curating elegant, airy, floral
-              designs that reflect your unique love story. At Flower with Grace,
-              we work collaboratively with you to help transform your vision
-              into reality, working to make sure each floral arrangement
-              reflects the utmost love and care that we instill into its design.
-            </p>
-
-            <article className="relative">
-              <Image
-                className="rounded-b-full w-[224px] h-[370px] object-cover float-end"
-                src={whoAreWe2}
-                alt="A pink floral arrangement in a round stone base sitting on top of a circular table with a pink sash."
-              />
-              <RoundedText
-                fill={"#fff"}
-                xPosition={"left-[-2rem]"}
-                yPosition={"top-[2rem]"}
-              />
-            </article>
-
-            <article className="flex flex-col gap-4">
-              <p className={`${averageSans.className} text-neutral-white`}>
-                We focus on the creation of thoughtful, signature pieces of the
-                highest quality. I handpick the best fresh flowers available in
-                the markets, to ensure they stay beautiful and lush for your big
-                day. I make an effort to present and discuss with each client a
-                variety of options to help best adapt to your budget. No matter
-                what option you choose, you and your guests can expect to see a
-                spectacular, thoughtfully-styled arrangement of high-quality
-                blooms.
-              </p>
-              <p className={`${averageSans.className} text-neutral-white`}>
-                Get in touch with me today to have a chat about your ideas and
-                see how we can transform your vision into reality!
-              </p>
-            </article>
+          <article className="relative">
+            <Image
+              className="rounded-t-full w-[224px] h-[370px] object-cover object-right"
+              src={whoAreWe1}
+              alt="A bride and groom stand intimately close to each other whilst looking into the other's eyes and holding a bouquet of white flowers."
+            />
+            <RoundedText
+              fill={"#fff"}
+              xPosition={"right-[-1rem]"}
+              yPosition={"top-[2rem]"}
+            />
           </article>
-        </section>
 
-        <CtaButton page={"/contact"}>Get in touch</CtaButton>
+          <p className={`${averageSans.className} text-neutral-white`}>
+            Based in North-West Sydney, Flower with Grace is a small yet highly
+            dedicated team focused on curating elegant, airy, floral designs
+            that reflect your unique love story. At Flower with Grace, we work
+            collaboratively with you to help transform your vision into reality,
+            working to make sure each floral arrangement reflects the utmost
+            love and care that we instill into its design.
+          </p>
+
+          <article className="relative">
+            <Image
+              className="rounded-b-full w-[224px] h-[370px] object-cover float-end"
+              src={whoAreWe2}
+              alt="A pink floral arrangement in a round stone base sitting on top of a circular table with a pink sash."
+            />
+            <RoundedText
+              fill={"#fff"}
+              xPosition={"left-[-2rem]"}
+              yPosition={"top-[2rem]"}
+            />
+          </article>
+
+          <article className="flex flex-col gap-4">
+            <p className={`${averageSans.className} text-neutral-white`}>
+              We focus on the creation of thoughtful, signature pieces of the
+              highest quality. I handpick the best fresh flowers available in
+              the markets, to ensure they stay beautiful and lush for your big
+              day. I make an effort to present and discuss with each client a
+              variety of options to help best adapt to your budget. No matter
+              what option you choose, you and your guests can expect to see a
+              spectacular, thoughtfully-styled arrangement of high-quality
+              blooms.
+            </p>
+            <p className={`${averageSans.className} text-neutral-white`}>
+              Get in touch with me today to have a chat about your ideas and see
+              how we can transform your vision into reality!
+            </p>
+          </article>
+        </article>
       </section>
-    </>
+
+      <CtaButton page={"/contact"}>Get in touch</CtaButton>
+    </section>
   );
 }
--- app/gallery/page.js
+++ app/gallery/page.js
@@ -1,3 +1,57 @@
+import gallery1 from "@/public/gallery-1.jpeg";
+import gallery2 from "@/public/gallery-2.jpeg";
+import gallery3 from "@/public/gallery-3.jpeg";
+import gallery4 from "@/public/gallery-4.jpeg";
+import gallery5 from "@/public/gallery-5.jpeg";
+import gallery6 from "@/public/gallery-6.jpeg";
+import Image from "next/image";
+
 export default function GalleryPage() {
-  return <h1>GalleryPage</h1>;
+  const images = [
+    {
+      id: 1,
+      src: gallery1,
+      alt: "A smiling bride stands outdoors holding a large bouquet of white roses and greenery. She is wearing a lace wedding dress with long, sheer sleeves featuring floral embroidery. Her dark, wavy hair is styled loosely, and a pearl necklace adorns her neck. The background shows a lush green hedge, adding a natural and serene atmosphere to the image",
+    },
+    {
+      id: 2,
+      src: gallery2,
+      alt: "A black-and-white photo captures a bride and groom running together, holding hands, with joyful energy. The bride's flowing gown and veil trail behind her as she lifts it slightly, revealing her heels. The groom is dressed in a suit and holds part of the bride's dress to assist her. The blurred motion adds a dynamic feel to the image. In the background, a grand building with tall columns and ivy-covered walls provides an elegant setting.",
+    },
+    {
+      id: 3,
+      src: gallery3,
+      alt: "A cheerful wedding party poses in front of an elegant building with tall white columns and brick walls. The bride stands at the center, radiant in a lace wedding gown, holding a bouquet of white roses. Surrounding her are five bridesmaids wearing navy-blue dresses with sparkling bodices and long, flowing skirts. Each bridesmaid holds a bouquet of white flowers, with some raising them playfully in the air. The group shares laughter and smiles, creating a joyful and celebratory scene. Lush greenery adds a touch of nature to the backdrop",
+    },
+    {
+      id: 4,
+      src: gallery4,
+      alt: "A bride stands gracefully on the steps of Cropley House, smiling as her bridesmaids tend to her elegant lace gown. The bridesmaids, dressed in navy-blue dresses with sparkling bodices, hold white bouquets while adjusting the bride’s train and veil. The grand entrance of the venue, with its white columns and arched doorway, creates a timeless and sophisticated backdrop. The warm lighting and natural expressions of joy add to the charm of this candid wedding moment.",
+    },
+    {
+      id: 5,
+      src: gallery5,
+      alt: "This black-and-white photograph captures a romantic moment between the bride and groom as they walk hand in hand. The bride's lace gown flows elegantly behind her, while the groom, dressed in a classic black suit, gazes at her lovingly. The natural setting, with towering trees and an open pathway, adds a timeless and cinematic feel to the image. The contrast and lighting create a nostalgic, almost dreamlike atmosphere, emphasizing the deep connection between the couple.",
+    },
+    {
+      id: 6,
+      src: gallery6,
+      alt: "This beautiful image captures the bride radiating happiness as she looks back with a bright smile. Her bridesmaids, dressed in elegant navy blue gowns with sparkling details, gracefully assist in carrying her flowing lace gown. The lush greenery and soft natural lighting enhance the romantic and joyful atmosphere of the moment. The candid composition perfectly conveys the excitement and warmth of the wedding day.",
+    },
+  ];
+
+  return (
+    <section className="bg-neutral-off-white px-5">
+      <article className="grid gap-4">
+        {images.map((image) => (
+          <Image
+            className="rounded-full"
+            key={image.id}
+            src={image.src}
+            alt={image.alt}
+          />
+        ))}
+      </article>
+    </section>
+  );
 }
